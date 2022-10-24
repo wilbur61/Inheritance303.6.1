893235bd721db6bd2014eb1c3f75093c907a072a
wilbur61 <wroales@perscholas.org>
2022-10-24 10:57:10 -0400

    Added my project

---
+++ src/Girl.java
@@ -0,0 +1,18 @@
+
+public class Girl extends Person {
+   static double   ageFactor = 1.5;
+   public Girl(String aName) {
+       name = "Ms." + aName;
+   }
+
+   public String talk() {
+       return("Hello! " + jump());
+   }
+   public String jump() {
+       return("I am jumping.");
+   }
+   public static double lifeSpan() {
+       return(lifeSpan * ageFactor);
+   }
+}
+
